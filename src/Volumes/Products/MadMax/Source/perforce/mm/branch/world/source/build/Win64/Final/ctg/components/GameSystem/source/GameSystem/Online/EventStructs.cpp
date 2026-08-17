6dbda82814aab37ae5ed1f8078b09a1c03fd983a
plyd823 <116851074+plyd823@users.noreply.github.com>
2026-08-16 22:00:32 -0400

    some quest stuff

--- src/Volumes/Products/MadMax/Source/perforce/mm/branch/world/source/build/Win64/Final/ctg/components/GameSystem/source/GameSystem/Online/EventStructs.cpp
+++ src/Volumes/Products/MadMax/Source/perforce/mm/branch/world/source/build/Win64/Final/ctg/components/GameSystem/source/GameSystem/Online/EventStructs.cpp
@@ -7,8 +7,8 @@ CHashString OpenWorldProgressionEvent::InstanceClassId() const {
     return CHashString(0x150b9ebc);
 }
 
-bool OpenWorldProgressionEvent::IsInstanceOf(CHashString a) const {
-    return SEventBaseData::IsInstanceOf(a);
+bool OpenWorldProgressionEvent::IsInstanceOf(CHashString classId) const {
+    return SEventBaseData::IsInstanceOf(classId);
 }
 
 SEventBaseData* OpenWorldProgressionEvent::Clone() const {
@@ -42,18 +42,18 @@ uint32_t OpenWorldProgressionEvent::GetIntegerField(CHashString fieldToCheck, bo
     foundField = true;
 
     switch (fieldToCheck.m_Hash) {
-            case 0x3f922b5d:
-                return this->total_active_count;
-            case 0x78f1e28e:
-                return this->total_world_count;
-            case 0xa14e22ab:
-                return this->location_item_type;
-            case 0x375b0af0:
-                return this->activity_tag;
-            default:
-                foundField = false;
-                return 0.0f;
-        }
+        case 0x3f922b5d:
+            return this->total_active_count;
+        case 0x78f1e28e:
+            return this->total_world_count;
+        case 0xa14e22ab:
+            return this->location_item_type;
+        case 0x375b0af0:
+            return this->activity_tag;
+        default:
+            foundField = false;
+            return 0.0f;
+    }
 }
 
 int32_t OpenWorldProgressionEvent::GetSignedIntegerField(CHashString fieldToCheck, bool& foundField) const {
@@ -73,5 +73,67 @@ CHashString OpenWorldProgressionEvent::ClassId() {
 
 }
 
+#endif
+
+#define BATCH_GAME_826130353_18_50
+#include "build/Win64/Final/ctg/components/GameSystem/include/GameSystem/Online/EventStructs_gen.h"
+
+#ifdef BATCH_GAME_826130353_18_50
+
+CHashString Quest::InstanceClassId() const {
+    return CHashString(0xb31d19ce);
+}
+
+bool Quest::IsInstanceOf(CHashString classId) const {
+    return SEventBaseData::IsInstanceOf(classId);
+}
+
+SEventBaseData* Quest::Clone() const {
+
+}
+
+const char* Quest::EventType() const {
+    return "Progression";
+}
+
+const char* Quest::EventName() const {
+    return "Quest";
+}
+
+float Quest::GetNumericalField(CHashString fieldToCheck) const {
+    switch (fieldToCheck.m_Hash) {
+        case 0x35468946:
+            return this->quest_id;
+        case 0x6ee2f4c1:
+            return this->quest_event_type;
+        case 0x5418bc88:
+            return this->quest_critical_path ? 1.0f : 0.0f;
+        default:
+            return 0.0f;
+    }
+}
+
+uint32_t Quest::GetIntegerField(CHashString fieldToCheck, bool& foundField) const {
+    foundField = true;
+
+    switch (fieldToCheck.m_Hash) {
+        case 0x35468946:
+            return this->quest_id;
+        case 0x6ee2f4c1:
+            return this->quest_event_type;
+        case 0x5418bc88:
+            return this->quest_critical_path;
+        case 0xc76776bb:
+            return CHashString(this->quest_name.c_str()).get();
+        default:
+            foundField = false;
+            return 0.0f;
+    }
+}
+
+int32_t Quest::GetSignedIntegerField(CHashString fieldToCheck, bool& foundField) const {
+    foundField = false;
+    return 0;
+}
 
 #endif
--- src/Volumes/Products/MadMax/Source/perforce/mm/branch/world/source/build/Win64/Final/project/Game/Logic/batch_Game_826130353_18_50.cxx
+++ src/Volumes/Products/MadMax/Source/perforce/mm/branch/world/source/build/Win64/Final/project/Game/Logic/batch_Game_826130353_18_50.cxx
@@ -1,2 +1,3 @@
 #define BATCH_GAME_826130353_18_50
 #include "project/Game/Mission/Mission.cpp"
+#include "build/Win64/Final/ctg/components/GameSystem/source/GameSystem/Online/EventStructs.cpp"
